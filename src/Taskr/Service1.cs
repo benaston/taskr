e67ccf15378e93e1faffba09840eb0cc83a6a7af
Ben Aston <ben.aston@rbi.co.uk>
2013-04-28 01:51:38 +0100

    Stuff

---
+++ src/Taskr/Service1.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace Taskr
+{
+    public partial class TaskrService : ServiceBase
+    {
+        public TaskrService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
